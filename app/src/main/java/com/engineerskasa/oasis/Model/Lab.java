2a436e59cd5f421467e24016249d405f256050da
KAY <slimshades68@gmail.com>
2020-07-04 13:14:46 +0000

    Locate Us

---
+++ app/src/main/java/com/engineerskasa/oasis/Model/Lab.java
@@ -0,0 +1,102 @@
+package com.engineerskasa.oasis.Model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "lab_request")
+public class Lab {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "gender")
+    private String gender;
+
+    @ColumnInfo(name = "age")
+    private String age;
+
+    @ColumnInfo(name = "doctor")
+    private String doctor;
+    @ColumnInfo(name = "description")
+    private String description;
+    @ColumnInfo(name = "phone")
+    private String phone;
+
+    public Lab() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Patients [id = "+id+"," +
+                " name = "+name+", " +
+                " doctor = "+doctor+", " +
+                " phone = "+phone+", " +
+                " age = "+age+", " +
+                " gender = "+gender+", " +
+                " description = "+description;
+
+    }
+}
