c37fd8268a3977f2fa2546910c3e88132053e08a
KAY <slimshades68@gmail.com>
2020-07-04 01:05:14 +0000

    Locate Us

--- app/src/main/java/com/engineerskasa/oasis/Activity/LocateUsActivity.java
+++ app/src/main/java/com/engineerskasa/oasis/Activity/LocateUsActivity.java
@@ -1,16 +1,88 @@
 package com.engineerskasa.oasis.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.engineerskasa.oasis.R;
+import com.engineerskasa.oasis.Utility.Tools;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class LocateUsActivity extends AppCompatActivity {
+public class LocateUsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private static final LatLng PERTH = new LatLng(37.926868, -78.024902);
+    private static final LatLng SYDNEY = new LatLng(5.6833, -0.1667);
+    private static final LatLng BRISBANE = new LatLng(-34, 151);
+
+
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locate_us);
+        Tools.setSystemBarColor(this, android.R.color.transparent);
+        Tools.setSystemBarLight(this);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                }).check();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(SYDNEY).title("MADINA"));
+        mMap.addMarker(new MarkerOptions().position(PERTH).title("Virginia"));
+        mMap.addMarker(new MarkerOptions().position(BRISBANE).title("SYDNEY"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(5.6833, -0.1667), 16.0f));
     }
 }
